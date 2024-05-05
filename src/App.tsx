c17a34adb38c1e36df2d4c4494dece7cedf5f658
Abdrakhman <abdrakhman@Abdrakhmans-MacBook-Pro.local>
2024-05-05 16:10:55 +0500

    feat: add io

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ function App() {
   const [code, setCode] = useState('#include<iostream>\n\nusing namespace std;\n\nint main() {\n\tcout << "Hello, World!";\n\treturn 0;\n}')
 
   return (
-    <main style={{ height: '100vh', width: '100vw'}}>
-      <div className='header'>
+    <main>
+      <CppEditor code={code} setCode={setCode} />
+      <div className='io'>
+        <textarea style={{ flex: 1  }}>
+
+        </textarea>
+        <div style={{ height: '100%', borderRight: '1px solid rgba(118, 118, 118, 0.4)'}}>
+
+        </div>
+        <textarea disabled style={{ flex: 1, }}>
+          Your output will be displayed here
+        </textarea>
+      </div>
+      <div className='footer'>
 				<button>
 					Run
 				</button>
-      </div>
-      <CppEditor code={code} setCode={setCode} />
-      <div>
-
       </div>
     </main>
   )
